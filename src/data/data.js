89bf97650a36d129e22cf443a85b9235b3280536
EphremAlemnew <ephremalemnew@gmail.com>
2025-01-22 10:23:51 +0300

    home update

--- src/data/data.js
+++ src/data/data.js
@@ -1,223 +1,247 @@
 export const techData = [
-    {
-      id: 1,
-      techName: "Flutter",
-      techImage: "https://images.seeklogo.com/logo-png/35/1/flutter-logo-png_seeklogo-354671.png?v=1957907673259129240",
-      expeYears: 3,
-    },
-    {
-      id: 2,
-      techName: "React",
-      techImage: "https://images.seeklogo.com/logo-png/27/1/react-logo-png_seeklogo-273845.png?v=1957908339984753648",
-      expeYears: 2,
-    },
-    {
-      id: 3,
-      techName: "Node.js",
-      techImage: "https://images.seeklogo.com/logo-png/27/1/node-js-logo-png_seeklogo-273750.png?v=1957907340654953160",
-      expeYears: 4,
-    },
-    {
-      id: 4,
-      techName: "Python",
-      techImage: "https://images.seeklogo.com/logo-png/27/1/python-logo-png_seeklogo-273830.png?v=1957908436346614776",
-      expeYears: 5,
-    },
-    {
-      id: 5,
-      techName: "Dart",
-      techImage: "https://seeklogo.com/images/D/dart-logo-FDA1939EC4-seeklogo.com.png",
-      expeYears: 2,
-    },
-    {
-      id: 6,
-      techName: "Next js",
-      techImage: "https://images.seeklogo.com/logo-png/32/1/next-js-logo-png_seeklogo-321806.png?v=1957908778386742672",
-      expeYears: 3,
-    },
-    {
-      id: 7,
-      techName: "React Native",
-      techImage: "https://images.seeklogo.com/logo-png/41/1/react-native-logo-png_seeklogo-410644.png?v=1957906344253720128",
-      expeYears: 2,
-    },
-    {
-      id: 8,
-      techName: "Django Rest Framework",
-      techImage: "https://www.django-rest-framework.org/img/logo.png",
-      expeYears: 4,
-    },
-    {
-      id: 9,
-      techName: "Java",
-      techImage: "https://seeklogo.com/images/J/java-logo-7833D1D21A-seeklogo.com.png",
-      expeYears: 5,
-    },
-    {
-      id: 10,
-      techName: "Spring Boot",
-      techImage: "https://images.seeklogo.com/logo-png/38/1/spring-boot-logo-png_seeklogo-385503.png?v=1957906706263657168",
-      expeYears: 2,
-    },
-    {
-      id: 11,
-      techName: "Chakra UI",
-      techImage: "https://chakra-ui-steps.vercel.app/54212428.png",
-      expeYears: 3,
-    },
-    {
-      id: 12,
-      techName: "Shadcn UI",
-      techImage: "https://images.seeklogo.com/logo-png/51/1/shadcn-ui-logo-png_seeklogo-519786.png?v=1957906705063198608",
-      expeYears: 2,
-    },
-    {
-      id: 13,
-      techName: "JavaScript",
-      techImage: "https://images.seeklogo.com/logo-png/27/1/javascript-js-logo-png_seeklogo-273557.png?v=1957908489043641400",
-      expeYears: 4,
-    },
-    {
-      id: 14,
-      techName: "Mongo DB",
-      techImage: "https://seeklogo.com/images/M/mongodb-logo-427DDF8FDE-seeklogo.com.png",
-      expeYears: 5,
-    },
-    {
-      id:15,
-      techName: "MySQL",
-      techImage: "https://seeklogo.com/images/M/MySQL-logo-F6FF285A58-seeklogo.com.png",
-      expeYears: 2,
-    },
-    {
-      id: 16,
-      techName: "Postgresql",
-      techImage: "https://images.seeklogo.com/logo-png/32/1/postgresql-logo-png_seeklogo-320016.png?v=1957906104778350904",
-      expeYears: 3,
-    },
-    {
-      id: 17,
-      techName: "Figma",
-      techImage: "https://images.seeklogo.com/logo-png/33/1/figma-logo-png_seeklogo-332042.png?v=1957908738128521496",
-      expeYears: 2,
-    },
-    {
-      id: 18,
-      techName: "Tailwind css",
-      techImage: "https://images.seeklogo.com/logo-png/35/1/tailwind-css-logo-png_seeklogo-354675.png?v=1957907763835204384",
-      expeYears: 4,
-    },
-    {
-      id: 19,
-      techName: "Docker",
-      techImage: "https://seeklogo.com/images/D/docker-logo-6D6F987702-seeklogo.com.png",
-      expeYears: 5,
-    },
-    {
-      id: 20,
-      techName: "Postman",
-      techImage: "https://images.seeklogo.com/logo-png/44/1/postman-api-platform-logo-png_seeklogo-446859.png?v=1957917574632890136",
-      expeYears: 2,
-    },
-    {
-      id: 21,
-      techName: "Github",
-      techImage: "https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png",
-      expeYears: 3,
-    },
-    {
-      id: 22,
-      techName: "Php",
-      techImage: "https://seeklogo.com/images/P/PHP-logo-0B2FDC4529-seeklogo.com.png",
-      expeYears: 2,
-    },
-    {
-      id: 23,
-      techName: "Express js",
-      techImage: "https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png",
-      expeYears: 4,
-    },
-    {
-      id: 24,
-      techName: "Apache",
-      techImage: "https://images.seeklogo.com/logo-png/31/1/apache-logo-png_seeklogo-314278.png?v=1957905405973103984",
-      expeYears: 5,
-    },
-    {
-      id: 25,
-      techName: "JWT",
-      techImage: "https://seeklogo.com/images/J/jwt-io-json-web-token-logo-40E02494CD-seeklogo.com.png",
-      expeYears: 2,
-    },
-    {
-      id: 26,
-      techName: "Notion",
-      techImage: "https://seeklogo.com/images/N/notion-app-logo-009B1538E8-seeklogo.com.png",
-      expeYears: 3,
-    },
-    {
-      id: 27,
-      techName: "VS code",
-      techImage: "https://images.seeklogo.com/logo-png/38/1/visual-studio-code-logo-png_seeklogo-384323.png?v=1957905886876500128",
-      expeYears: 2,
-    },
-    // {
-    //   id: 28,
-    //   techName: "Django Rest Framework",
-    //   techImage: "https://www.django-rest-framework.org/img/logo.png",
-    //   expeYears: 4,
-    // },
-    
-  ];
-  export const projectsData = [
-    {
-      id: 1,
-      name: "Calculator",
-      image: "/Calculator.png",
-      proUrl: "https://calculator-uio5.onrender.com/",
-      proTech: "React, Vite.js, Chakra UI",
-      codeUrl: "https://github.com/EphremAlemnew/Calculator",
-    },
-    {
-      id: 2,
-      name: "Toorax Inventory Management System",
-      image: "/Toorax.png",
-      proUrl: "",
-      proTech: "Next.js, Django restframework, Shadcn",
-      codeUrl: "https://github.com/EphremAlemnew",
-    },
-    {
-      id: 3,
-      name: "Simple To do list app",
-      image: "/Flutter.jpg",
-      proUrl: "",
-      proTech: "Flutter, Dart",
-      codeUrl: "https://github.com/EphremAlemnew/Flutter-Simple-to-do-list-app.git",
-    },
-    {
-      id: 4,
-      name: "Online Medication Finder",
-      proUrl: "",
-      image: "/Medication.jpg",
-      proTech: "React-Native, Django rest framework",
-      codeUrl: "https://github.com/EphremAlemnew",
-    },
-    {
-      id: 5,
-      name: "Simple Ecommerce Web app",
-      image: "/SimpleEcommerce.png",
-      proUrl: "",
-      proTech: "Mongodb, React, Express.js, Node.js",
-      codeUrl: "https://github.com/EphremAlemnew/MernSimpleWeb",
-    },
-    // {
-    //   id: 5,
-    //   name: "Calculator",
-    //   image: "/Calculator.png",
-    //   proUrl: "https://calculator-uio5.onrender.com/",
-    //   proTech: "React, Vite.js, Chakra UI",
-    //   codeUrl: "https://github.com/EphremAlemnew/Calculator",
-    // },
-    // Other projects
-  ];
-  
+  {
+    id: 1,
+    techName: "Flutter",
+    techImage:
+      "https://images.seeklogo.com/logo-png/35/1/flutter-logo-png_seeklogo-354671.png?v=1957907673259129240",
+    expeYears: 3,
+  },
+  {
+    id: 2,
+    techName: "React",
+    techImage:
+      "https://images.seeklogo.com/logo-png/27/1/react-logo-png_seeklogo-273845.png?v=1957908339984753648",
+    expeYears: 2,
+  },
+  {
+    id: 3,
+    techName: "Node.js",
+    techImage:
+      "https://images.seeklogo.com/logo-png/27/1/node-js-logo-png_seeklogo-273750.png?v=1957907340654953160",
+    expeYears: 4,
+  },
+  {
+    id: 4,
+    techName: "Python",
+    techImage:
+      "https://images.seeklogo.com/logo-png/27/1/python-logo-png_seeklogo-273830.png?v=1957908436346614776",
+    expeYears: 5,
+  },
+  {
+    id: 5,
+    techName: "Dart",
+    techImage:
+      "https://seeklogo.com/images/D/dart-logo-FDA1939EC4-seeklogo.com.png",
+    expeYears: 2,
+  },
+  {
+    id: 6,
+    techName: "Next js",
+    techImage:
+      "https://images.seeklogo.com/logo-png/32/1/next-js-logo-png_seeklogo-321806.png?v=1957908778386742672",
+    expeYears: 3,
+  },
+  {
+    id: 7,
+    techName: "React Native",
+    techImage:
+      "https://images.seeklogo.com/logo-png/41/1/react-native-logo-png_seeklogo-410644.png?v=1957906344253720128",
+    expeYears: 2,
+  },
+  {
+    id: 8,
+    techName: "Django Rest Framework",
+    techImage: "https://www.django-rest-framework.org/img/logo.png",
+    expeYears: 4,
+  },
+  {
+    id: 9,
+    techName: "Java",
+    techImage:
+      "https://seeklogo.com/images/J/java-logo-7833D1D21A-seeklogo.com.png",
+    expeYears: 5,
+  },
+  {
+    id: 10,
+    techName: "Spring Boot",
+    techImage:
+      "https://images.seeklogo.com/logo-png/38/1/spring-boot-logo-png_seeklogo-385503.png?v=1957906706263657168",
+    expeYears: 2,
+  },
+  {
+    id: 11,
+    techName: "Chakra UI",
+    techImage: "https://chakra-ui-steps.vercel.app/54212428.png",
+    expeYears: 3,
+  },
+  {
+    id: 12,
+    techName: "Shadcn UI",
+    techImage:
+      "https://images.seeklogo.com/logo-png/51/1/shadcn-ui-logo-png_seeklogo-519786.png?v=1957906705063198608",
+    expeYears: 2,
+  },
+  {
+    id: 13,
+    techName: "JavaScript",
+    techImage:
+      "https://images.seeklogo.com/logo-png/27/1/javascript-js-logo-png_seeklogo-273557.png?v=1957908489043641400",
+    expeYears: 4,
+  },
+  {
+    id: 14,
+    techName: "Mongo DB",
+    techImage:
+      "https://seeklogo.com/images/M/mongodb-logo-427DDF8FDE-seeklogo.com.png",
+    expeYears: 5,
+  },
+  {
+    id: 15,
+    techName: "MySQL",
+    techImage:
+      "https://seeklogo.com/images/M/MySQL-logo-F6FF285A58-seeklogo.com.png",
+    expeYears: 2,
+  },
+  {
+    id: 16,
+    techName: "Postgresql",
+    techImage:
+      "https://images.seeklogo.com/logo-png/32/1/postgresql-logo-png_seeklogo-320016.png?v=1957906104778350904",
+    expeYears: 3,
+  },
+  {
+    id: 17,
+    techName: "Figma",
+    techImage:
+      "https://images.seeklogo.com/logo-png/33/1/figma-logo-png_seeklogo-332042.png?v=1957908738128521496",
+    expeYears: 2,
+  },
+  {
+    id: 18,
+    techName: "Tailwind css",
+    techImage:
+      "https://images.seeklogo.com/logo-png/35/1/tailwind-css-logo-png_seeklogo-354675.png?v=1957907763835204384",
+    expeYears: 4,
+  },
+  {
+    id: 19,
+    techName: "Docker",
+    techImage:
+      "https://seeklogo.com/images/D/docker-logo-6D6F987702-seeklogo.com.png",
+    expeYears: 5,
+  },
+  {
+    id: 20,
+    techName: "Postman",
+    techImage:
+      "https://images.seeklogo.com/logo-png/44/1/postman-api-platform-logo-png_seeklogo-446859.png?v=1957917574632890136",
+    expeYears: 2,
+  },
+  {
+    id: 21,
+    techName: "Github",
+    techImage:
+      "https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png",
+    expeYears: 3,
+  },
+  {
+    id: 22,
+    techName: "Php",
+    techImage:
+      "https://seeklogo.com/images/P/PHP-logo-0B2FDC4529-seeklogo.com.png",
+    expeYears: 2,
+  },
+  {
+    id: 23,
+    techName: "Express js",
+    techImage:
+      "https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png",
+    expeYears: 4,
+  },
+  {
+    id: 24,
+    techName: "Apache",
+    techImage:
+      "https://images.seeklogo.com/logo-png/31/1/apache-logo-png_seeklogo-314278.png?v=1957905405973103984",
+    expeYears: 5,
+  },
+  {
+    id: 25,
+    techName: "JWT",
+    techImage:
+      "https://seeklogo.com/images/J/jwt-io-json-web-token-logo-40E02494CD-seeklogo.com.png",
+    expeYears: 2,
+  },
+  {
+    id: 26,
+    techName: "Notion",
+    techImage:
+      "https://seeklogo.com/images/N/notion-app-logo-009B1538E8-seeklogo.com.png",
+    expeYears: 3,
+  },
+  {
+    id: 27,
+    techName: "VS code",
+    techImage:
+      "https://images.seeklogo.com/logo-png/38/1/visual-studio-code-logo-png_seeklogo-384323.png?v=1957905886876500128",
+    expeYears: 2,
+  },
+  // {
+  //   id: 28,
+  //   techName: "Django Rest Framework",
+  //   techImage: "https://www.django-rest-framework.org/img/logo.png",
+  //   expeYears: 4,
+  // },
+];
+export const projectsData = [
+  {
+    id: 1,
+    name: "Calculator",
+    image: "/Calculator.png",
+    proUrl: "https://calculator-uio5.onrender.com/",
+    proTech: "React, Vite.js, Chakra UI",
+    codeUrl: "https://github.com/EphremAlemnew/Calculator",
+  },
+  {
+    id: 2,
+    name: "Toorax Inventory Management System",
+    image: "/Toorax.png",
+    proUrl: "",
+    proTech: "Next.js, Django restframework, Shadcn",
+    codeUrl: "https://github.com/EphremAlemnew",
+  },
+  {
+    id: 3,
+    name: "Simple To do list app",
+    image: "/Flutter.jpg",
+    proUrl: "",
+    proTech: "Flutter, Dart",
+    codeUrl:
+      "https://github.com/EphremAlemnew/Flutter-Simple-to-do-list-app.git",
+  },
+  {
+    id: 4,
+    name: "Online Medication Finder",
+    proUrl: "",
+    image: "/Medication.jpg",
+    proTech: "React-Native, Django rest framework",
+    codeUrl: "https://github.com/EphremAlemnew",
+  },
+  {
+    id: 5,
+    name: "Simple Ecommerce Web app",
+    image: "/SimpleEcommerce.png",
+    proUrl: "",
+    proTech: "Mongodb, React, Express.js, Node.js",
+    codeUrl: "https://github.com/EphremAlemnew/MernSimpleWeb",
+  },
+  {
+    id: 6,
+    name: "Simple Home page",
+    image: "/Home.png",
+    proUrl: "https://homepage-ljq4.onrender.com/",
+    proTech: "React, Vite.js, Chakra UI",
+    codeUrl: "https://github.com/EphremAlemnew/ConduktorHomePage",
+  },
+  // Other projects
+];
